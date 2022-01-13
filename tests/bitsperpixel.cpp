59d9eb2aeecf69c2437d8f1d9760c3df2fa8b488
David Capello <davidcapello@gmail.com>
2022-01-12 21:13:38 -0300

    Test image iterators

--- tests/bitsperpixel.cpp
+++ tests/bitsperpixel.cpp
@@ -1,10 +1,27 @@
 // Raster Library
-// Copyright (C) 2015 David Capello
+// Copyright (C) 2015-2022 David Capello
 
 #include "raster/raster.h"
 #include "test.h"
 
+#include <vector>
+
 using namespace raster;
+
+template<typename Pixel>
+void expect_array(image_view& img, const std::vector<typename Pixel::value_type>& values)
+{
+  auto it = values.begin(), end = values.end();
+  auto specific_img = img.view_as<Pixel>();
+  int x = 0;
+  for (auto p : specific_img) {
+    printf("x=%d\n", x++);
+    EXPECT_EQ(true, it != end);
+    EXPECT_EQ((int)*it, (int)p);
+    ++it;
+  }
+  EXPECT_EQ(true, it == end);
+}
 
 int main() {
   image_spec spec(2, 1, 32);
@@ -17,6 +34,7 @@ int main() {
   put_pixel(img32, 1, 0, 0x80010032);
   EXPECT_EQ(0x00FF001E, get_pixel(img32, 0, 0));
   EXPECT_EQ(0x80010032, get_pixel(img32, 1, 0));
+  expect_array<Pixel32bpp>(img32, { 0x00FF001E, 0x80010032 });
 
   // 001E 00FF 0032 8001
   image_view img16(image_spec(4, 1, 16), buf);
@@ -24,6 +42,7 @@ int main() {
   EXPECT_EQ(0x00FF, get_pixel(img16, 1, 0));
   EXPECT_EQ(0x0032, get_pixel(img16, 2, 0));
   EXPECT_EQ(0x8001, get_pixel(img16, 3, 0));
+  expect_array<Pixel16bpp>(img16, { 0x001E, 0x00FF, 0x0032, 0x8001 });
 
   // 1E 00 FF 00 32 00 01 80
   image_view img8(image_spec(8, 1, 8), buf);
@@ -35,6 +54,7 @@ int main() {
   EXPECT_EQ(0x00, get_pixel(img8, 5, 0));
   EXPECT_EQ(0x01, get_pixel(img8, 6, 0));
   EXPECT_EQ(0x80, get_pixel(img8, 7, 0));
+  expect_array<Pixel8bpp>(img8, { 0x1E, 0x00, 0xFF, 0x00, 0x32, 0x00, 0x01, 0x80 });
 
   // E 1 0 0 F F 0 0 2 3 0 0 1 0 0 8
   image_view img4(image_spec(16, 1, 4), buf);
